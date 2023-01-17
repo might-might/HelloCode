21602781460d753833a92399f89dc1adb2556983
might_might <bcmactive@gmail.com>
2023-01-17 17:55:34 +0300

    12th exmpl added

---
+++ Examples012_Methods/Program.cs
@@ -0,0 +1,53 @@
+﻿// Вид 1 - например, указание авторства
+
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+Method1();
+
+// Вид 2
+
+void Method2(string msg1)
+{
+    Console.WriteLine(msg1);
+}
+Method2("Текст сообщения 1");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21(count: 4, msg: "Текст сообщения");
+
+// Вид 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+// Вид 4
+
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
